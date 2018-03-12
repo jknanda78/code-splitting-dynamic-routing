d6e934eeb1552a4574ac6858859a071a7e249043
jnanda <jyoti.kaustuv.nanda@gmail.com>
2018-03-11 19:41:34 -0700

    Updates

---
+++ static/js/Openings.2f0a48e3.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{142:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l=t(0),r=t.n(l);n.default=function(){return r.a.createElement("div",null,r.a.createElement("h2",null,"Current Openings"),r.a.createElement("p",null,"Content for Openings"))}}});
+//# sourceMappingURL=Openings.2f0a48e3.chunk.js.map
