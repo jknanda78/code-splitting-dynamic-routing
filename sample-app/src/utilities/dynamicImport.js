8b68e6324c3dfa06ff791386727f9d2ede61be55
jnanda <jyoti.kaustuv.nanda@gmail.com>
2018-03-22 19:41:53 -0700

    hack for browser refresh + url param example

--- sample-app/src/utilities/dynamicImport.js
+++ sample-app/src/utilities/dynamicImport.js
@@ -2,9 +2,15 @@ import { Component } from 'react';
 import { addRoutes, navigateToScreen } from '../app/actions/creators';
 import store, { injectReducers } from '../app/store';
 
+/**
+ * Hack for Browser refresh.
+ * Sub-Routes needs to load asynchronously
+ */
 window.onload = function() {
   const path = window.location.pathname;
-  if (path.split('/').length > 2) {
+  const len = process.env.NODE_ENV === 'development' ? 2 : 3;
+
+  if (path.split('/').length > len) {
     store.dispatch(navigateToScreen(path));
   }
 };
